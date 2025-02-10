09fd3590de7b4b96cfcf2122eb03bc96d654759a
MuuSeoTia <tiahimouad22@gmail.com>
2025-02-10 17:14:13 -0500

    Fixed project structure for website deployment

---
+++ app/src/CSS.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+module CSS where
+
+import Clay hiding (span, map)
+import qualified Clay.Elements as E
+import Data.Text.Lazy (Text)
+import qualified Clay.Render as R
+import Prelude hiding (span)
+import qualified Prelude as P
+
+// ... rest of your existing CSS.hs content ... 
